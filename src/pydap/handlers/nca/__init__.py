81dc40bf304755de6247986b7869017d8cb19a62
Roberto De Almeida <roberto@dealmeida.net>
2013-06-25 14:52:31 -0700

    Importing new version for Pydap 3.2

--- src/pydap/handlers/nca/__init__.py
+++ src/pydap/handlers/nca/__init__.py
@@ -1,237 +1,251 @@
-"""
-A NetCDF aggregator.
-
-Suppose we have several model outputs in files output1.nc, output2.nc,
-etc., and we want to combine them into a single dataset by creating
-a new axis called "ensemble". This would be done by creating the
-following file with the extension ``.nca``::
-
-    [dataset]
-    ; obligatory parameters
-    match = output*.nc
-    axis = ensemble
-    ; optional metadata
-    name = MOM4_ensemble
-    history = Ensemble members from a simulation using MOM4
-    
-    [ensemble]
-    values = 1, 2, ...
-    ; optional metadata
-    units = member
-
-Values can be specified using 3 different syntaxes::
-
-    values = 1, 2, ...
-    values = 1.0, ..., 10.0
-    values = 1, 2, 3, 4, 5, 6
-
-The first example assigns a value of 1 for the first point, 2 for
-the second, and so on, with a delta of 1 up to the last point. The
-second example assigns a value of 1.0 for the first point, 10.0 for
-the last, and evenly distributed values in between, depending on
-the number of files. The last one explicitly assigns values to each
-point.
-
-Another example: we want to combine monthly output in a single
-dataset. This is done by aggregating along an existing axis (say,
-"time")::
-
-    [dataset]
-    name = annual
-    match = output2009??.nc
-    axis = time
-
-The aggregation is done lazily, ie, it avoids having to read data
-from all files into memory in order to build the new dataset. Data
-is only read as necessary.
-
-"""
-
-import re
-import os.path
+import os
 from glob import glob
-import string
+import re
+import time
+from stat import ST_MTIME
+from email.utils import formatdate
+from ast import literal_eval as eval
 
-from numpy import asarray, swapaxes, linspace, concatenate, arange
+import numpy as np
 from configobj import ConfigObj
+
 from pupynere import netcdf_file
 
 from pydap.model import *
-from pydap.lib import combine_slices, fix_slice
 from pydap.handlers.lib import BaseHandler
-from pydap.handlers.helper import constrain
-from pydap.responses.das import get_type
-from pydap.util.safeeval import expr_eval
+from pydap.exceptions import OpenFileError
+from pydap.lib import fix_slice
 
 
-def lazy_eval(s):
+class FileAttributeCache(object):
     """
-    Try to evalute expression or fallback to string.
-    
-        >>> lazy_eval("1")
-        1
-        >>> lazy_eval("a")
-        'a'
+    Cache for storing and retrieving values tied to files which can change.
+
+    This is used so we don't need to open NetCDF files for each request to get
+    data from the aggregation axis.
 
     """
-    try:
-        s = expr_eval(s)
-    except:
-        pass
-    return s
+    def __init__(self):
+        self.cache = {}
+        self.mtime = {}
+
+    def __setitem__(self, filepath, value):
+        """
+        Assign a value to the file `filepath`.
+
+        """
+        self.cache[filepath] = value
+        self.mtime[filepath] = os.stat(filepath)[ST_MTIME]
+
+    def __getitem__(self, filepath):
+        """
+        Try to retrieve the value associated with `filepath`.
+
+        This will fail if the file has been modified.
+
+        """
+        if filepath not in self.cache:
+            raise KeyError
+
+        mtime = os.stat(filepath)[ST_MTIME]
+        if mtime > self.mtime[filepath]:
+            del self.cache[filepath]
+            raise KeyError
+
+        return self.cache[filepath]
 
 
-class Handler(BaseHandler):
+class PseudoNetCDFVariable(object):
+    """
+    Create a pseudo NetCDF variable.
+
+    This is used to create the aggregation axis as if it were a variable in the
+    original NetCDF files. Basically we need just to return data and attributes.
+
+    """
+    def __init__(self, data, attributes):
+        self.data = data
+        self._attributes = attributes
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
+class NetCDFAggregatorHandler(BaseHandler):
 
     extensions = re.compile(r"^.*\.nca$", re.IGNORECASE)
 
     def __init__(self, filepath):
-        config = ConfigObj(filepath)
+        BaseHandler.__init__(self)
+
+        self.filepath = filepath
+        self.cache = FileAttributeCache()
+
+    def parse(self, projection, selection):
+        """
+        Aggregation must be done per request, since new files may be added.
+
+        """
+        try:
+            config = ConfigObj(self.filepath)
+        except Exception, exc:
+            message = 'Unable to open file %s: %s' % (self.filepath, exc)
+            raise OpenFileError(message)
+
+        # get list of files
         files = glob(config['dataset']['match'])
-        template = netcdf_file(files[0])  # use first file as a template
-        to_close = [ template ]           # handlers we need to close later
+        files.sort()
 
-        # Create dataset and populate with metadata from the INI file.
-        self.dataset = DatasetType(
-                name=config['dataset'].get('name', os.path.split(filepath)[1]),
-                attributes=dict((k, v) for (k, v) in config['dataset'].items()
-                    if k not in ['name', 'match', 'axis']))
-        self.dataset.attributes['NC_GLOBAL'] = template._attributes.copy()
+        # add last-modified header, taking the last modified file from the list
+        mtime = max(os.stat(file)[ST_MTIME] for file in files)
+        self.additional_headers = [ (k, v) for (k, v) in self.additional_headers if
+                k != 'Last-modified' ]
+        self.additional_headers.append(
+                ('Last-modified', (formatdate(time.mktime(time.localtime(mtime))))))
 
-        # Build aggregation axis.
-        name = config['dataset']['axis']
-        if name in config:
-            # Create a new axis.
-            data = parse_values(config[name]['values'], len(files))
-            type_ = get_type(data)
-            attributes = dict((k, v) for (k, v) in config[name].items()
-                    if k != 'values')
-        elif name in template.variables:
-            # Combine existing axes into a single one.
+        # get a template
+        template = netcdf_file(files[0])
+        vars = template.variables
+        dims = template.dimensions
+
+        # build aggregation axis
+        axis = config['dataset']['axis']
+        if axis in config:
+            # create a new axis
+            values = config[axis]['values']
+            data = parse_values(values, len(files))
+            attributes = { k:v for k, v in config[axis].items() if k != 'values' }
+            count = [ (file, 0) for file in files ]
+        elif axis in dims:
+            # aggregate along existing axis
+            count = []
             data = []
             for file in files:
-                f = netcdf_file(file)
-                to_close.append(f)
-                data.extend(f.variables[name][:])
-            data = asarray(data)
-            type_ = template.variables[name].typecode()
-            attributes = template.variables[name]._attributes.copy()
+                try:
+                    values = self.cache[file]
+                except KeyError:
+                    f = netcdf_file(file)
+                    values = f.variables[axis][:]
+                    f.close()
+                    self.cache[file] = values
+                data.extend(values)
+                count.append((file, len(values)))
+            data = np.array(data)
+            attributes = vars[axis]._attributes
         else:
             raise Exception('Invalid axis definition "%s".' % name)
-        agg_axis = BaseType(name=name, data=data, type=type_, shape=data.shape, attributes=attributes)
 
-        # Add grids to the dataset.
-        for name in [name for name in template.variables
-                if name not in template.dimensions]:
-            var = template.variables[name]
-            self.dataset[name] = grid = GridType(name=name, attributes=var._attributes.copy())
-            grid[name] = BaseType(name=name, type=type_, attributes=var._attributes.copy())
+        # add new axis, or replace if existing
+        dims[axis] = len(data)
+        vars[axis] = PseudoNetCDFVariable(data, attributes)
 
-            # Prepare aggregated data.
-            if agg_axis.name not in var.dimensions:
-                axis = None
-                self.dataset[agg_axis.name] = grid[agg_axis.name] = agg_axis
-                grid[name].dimensions = (agg_axis.name,) + var.dimensions
+        # build dataset
+        name = os.path.split(self.filepath)[1]
+        self.dataset = DatasetType(name, attributes=dict(NC_GLOBAL=template._attributes))
+        for dim in dims:
+            if dims[dim] is None:
+                #self.dataset.attributes['DODS_EXTRA'] = {'Unlimited_Dimension': dim}
+                break
+
+        # add grids
+        grids = [var for var in vars if var not in dims]
+        for grid in grids:
+            # should we add the axis to the dimensions?
+            if axis not in vars[grid].dimensions:
+                vars[grid].dimensions = (axis,) + vars[grid].dimensions
+                shape = (dims[axis],) + vars[grid].shape
+                index = None
             else:
-                axis = list(var.dimensions).index(agg_axis.name)
-                grid[name].dimensions = var.dimensions
-            fs = [netcdf_file(file) for file in files]
-            to_close.extend(fs)
-            grid[name].data = LazyConcatenator(
-                    [f.variables[name] for f in fs],
-                    axis=axis)
-            grid[name].shape = grid[name].data.shape
+                shape = list(vars[grid].shape)
+                index = vars[grid].dimensions.index(axis)
+                shape[index] = dims[axis]
+                shape = tuple(shape)
+            dtype = np.dtype(vars[grid].typecode())
 
-            # Add axes for the dimensions.
-            for dim in var.dimensions:
-                if dim == agg_axis.name:
-                    # replace axis with aggregated axis
-                    self.dataset[dim] = grid[dim] = agg_axis
-                else:
-                    # add a regular axis
-                    self.dataset[dim] = grid[dim] = BaseType(name=dim,
-                            data=template.variables[dim][:],
-                            type=template.variables[dim].typecode(),
-                            shape=template.variables[dim].shape,
-                            attributes=template.variables[dim]._attributes.copy())
+            # create grid
+            self.dataset[grid] = GridType(grid, vars[grid]._attributes)
 
-        # Close all open files.
-        self.dataset.close = lambda: [f.close() for f in to_close]
+            # add array
+            self.dataset[grid][grid] = BaseType(grid,
+                    AggregateNetcdfData(grid, index, count, dtype, shape),
+                    vars[grid].dimensions, vars[grid]._attributes)
 
-    def parse_constraints(self, environ):
-        # Let's use the automatic constraint evaluator.
-        new_dataset = constrain(self.dataset, environ.get('QUERY_STRING', ''))
-        if hasattr(self.dataset, 'close'): self.dataset.close()
-        return new_dataset
+            # add maps
+            for dim in vars[grid].dimensions:
+                self.dataset[grid][dim] = BaseType(dim, vars[dim][:],
+                        None, vars[dim]._attributes)
+
+        # add dims
+        for dim in dims:
+            self.dataset[dim] = BaseType(dim, vars[dim][:],
+                    None, vars[dim]._attributes)
+
+        template.close()
+
+        return BaseHandler.parse(self, projection, selection)
 
 
-class LazyConcatenator(object):
+class AggregateNetcdfData(object):
     """
-    A lazy concatenator.
-
-    This class is similar to the ``concatenate`` function from Numpy,
-    except that the new object is a "view" of the concatenated arrays,
-    and data is only read when this object is sliced.
+    Aggregates variables from NetCDF files along a given axis.
 
     """
-    def __init__(self, arrays, axis=None):
-        if axis is None:
-            # Create a new axis
-            self.shape = (len(arrays),) + arrays[0].shape
-            self._arrays = asarray(arrays, 'O') 
-        else:
-            # 'cat along a given axis
-            shape = list(arrays[0].shape)
-            shape[axis] = sum([array.shape[axis] for array in arrays])
-            self.shape = tuple(shape)
-            self._arrays = asarray(
-                    [LazySlice((slice(None),)*axis+(i,), arrays[j]) 
-                        for j in range(len(arrays))
-                        for i in range(arrays[j].shape[axis])],
-                    'O')
-        self._axis = axis
+    def __init__(self, name, axis, count, dtype, shape):
+        self.name = name
+        self.axis = axis
+        self.count = count 
+        self.dtype = dtype
+        self.shape = shape
 
     def __getitem__(self, index):
-        # Extract the slice for the object array
         index = fix_slice(index, self.shape)
-        if self._axis is None:
-            select, index = index[0], index[1:]
-            out = asarray(
-                    [obj[index] for obj in self._arrays[select]])
+
+        # create a new axis
+        if self.axis is None:
+            # get the slice along the aggregation axis, and leave the rest for
+            # the variable itself
+            slice_, index = index[0], index[1:]
+            data = []
+            for file, n in self.count[slice_]:
+                f = netcdf_file(file)
+                data.append(f.variables[self.name][index])
+                f.close()
+            return np.array(data).astype(self.dtype)
+
+        # concatenate along an existing axis
         else:
-            select = index[self._axis]
-            out = concatenate(
-                    [obj[index] for obj in self._arrays[select]],
-                    axis=self._axis)
-        return out
+            # convert index to list so we can change it
+            index = list(index) 
 
-    @property
-    def __array_interface__(self):
-        data = self[:]
-        return {
-                'version': 3,
-                'shape': data.shape,
-                'typestr': data.dtype.str,
-                'data': data,
-                }
+            # get the slice along the aggregation axis and store it in a 
+            # boolean array that we'll map to the files
+            slice_ = index[self.axis]
+            indexes = np.zeros(self.shape[self.axis], bool)
+            indexes[slice_] = 1
 
+            offset = 0
+            data = []
+            for file, n in self.count:
+                selected_here = indexes[offset:offset+n]
+                if any(selected_here):
+                    index[self.axis] = selected_here
+                    f = netcdf_file(file)
+                    data.append(f.variables[self.name][tuple(index)])
+                    f.close()
+                offset += n
+            return np.concatenate(data, axis=self.axis).astype(self.dtype)
 
-class LazySlice(object):
-    """
-    A lazy slice.
+    # Comparisons are passed to the data.
+    def __eq__(self, other): return self[:] == other
+    def __ne__(self, other): return self[:] != other
+    def __ge__(self, other): return self[:] >= other
+    def __le__(self, other): return self[:] <= other
+    def __gt__(self, other): return self[:] > other
+    def __lt__(self, other): return self[:] < other
 
-    This is a lazy slice. The initial slice is applied only when
-    the object is sliced again; both slices are then combined
-    and applied to the data.
-
-    """
-    def __init__(self, slice, obj):
-        self.slice = fix_slice(slice, obj.shape)
-        self.obj = obj
-
-    def __getitem__(self, index):
-        index = fix_slice(index, self.obj.shape)
-        return self.obj[combine_slices(index, self.slice)]
+    # Implement the sequence and iter protocols.
+    def __len__(self): return self.shape[0]
+    def __iter__(self): return iter(self[:])    
 
 
 def parse_values(input, size):
@@ -248,20 +262,20 @@ def parse_values(input, size):
 
     """
     if len(input) == size and "..." not in input:
-        return asarray(map(lazy_eval, input))
+        return np.asarray(map(eval, input))
     start, next, stop = input[:]
     if next == '...':
-        return linspace(float(start), float(stop), size)
+        return np.linspace(eval(start), eval(stop), size)
     elif stop == '...':
-        dx = float(next)-float(start)
-        return arange(float(start), float(start)+size*dx, dx)
+        dx = eval(next)-eval(start)
+        return np.arange(eval(start), eval(start)+size*dx, dx)
     else:
         raise Exception('Unable to parse: %s' % input)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    from paste.httpserver import serve
+    from werkzeug.serving import run_simple
 
-    application = Handler(sys.argv[1])
-    serve(application, port=8001)
+    application = NetCDFAggregatorHandler(sys.argv[1])
+    run_simple('localhost', 8001, application, use_reloader=True)
